d6b112c490a70e2a85af47dcdc4fa969f5c89d7d
sjdimarco <64240002+sjdimarco@users.noreply.github.com>
2020-04-25 18:37:21 -0600

    crear pagina para escuela
    subir los cambios de la escuela

---
+++ escuela.php
@@ -0,0 +1,23 @@
+<?php
+include('include/ini.php');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="utf-8">
+	<title>ADAES Costa Rica</title>
+	<link rel="stylesheet" href="css/styles.css"  type="text/css"  />
+</head>
+<body>
+<?php include 'include/header.php';?>
+<section id="main-content">
+	<form method="POST" action="contactenos.php">
+		<div class="content">
+			<h1>Pagina en Construcción</h1>
+		</div>
+	</form>
+</section> <!-- / #main-content -->
+<?php include 'include/footer.php';
+?>
+</body>
+</html>
